ba5141a291b573e2f116b1d8154c90b7bcf34776
shaankeole <skeole24@brophybroncos.org>
2022-02-24 14:56:46 -0700

    Platformer

--- Platformer_V1.py
+++ Platformer_V1.py
@@ -7,15 +7,18 @@ pygame.init()
 gameDisplay = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 
-box_1 = box.Box(50, Values.black, gameDisplay)
+box_1 = box.Box(100, Values.black, gameDisplay)
 box_1.move(400, 200, 0)
 
 background_1 = background.Background(gameDisplay)
+background_1.update_hitbox()
 
 run = True
 pause = False
 x_vel = 0.0
 y_vel = 0.0
+
+r = 0.75 / box_1.size
 
 while run:
     for event in pygame.event.get():
@@ -25,20 +28,44 @@ while run:
             pause = not pause
 
     if pygame.key.get_pressed()[pygame.K_RIGHT]:
-        x_vel += 2
+        x_vel += 1
     if pygame.key.get_pressed()[pygame.K_LEFT]:
-        x_vel -= 2
+        x_vel -= 1
     x_vel *= 0.9
+    box_1.x += x_vel
+    box_1.angle += x_vel * r
+    box_1.update_hitbox()
 
-    if pygame.key.get_pressed()[pygame.K_SPACE]:
-        y_vel -= 2
+    count = 0
+    for i in range(15):
+        if Values.hitboxes_intersect(box_1.hitbox, background_1.hitbox):
+            box_1.y -= 1
+            box_1.update_hitbox()
+    if count == 15:
+        box_1.y += 15
+        box_1.angle -= x_vel * r
+        box_1.x -= x_vel
+        x_vel = 0
+        box_1.update_hitbox()
+
     y_vel += 1
-    y_vel += 0.1
+    box_1.y += y_vel
+    box_1.update_hitbox()
+    if Values.hitboxes_intersect(box_1.hitbox, background_1.hitbox):
+        box_1.y -= y_vel
+        y_vel = 0
+    box_1.y += 1
+    box_1.update_hitbox()
+    if Values.hitboxes_intersect(box_1.hitbox, background_1.hitbox) and (pygame.key.get_pressed()[pygame.K_SPACE] or pygame.key.get_pressed()[pygame.K_UP]):
+        y_vel = -15
+    box_1.y -= 1
+    box_1.update_hitbox()
 
-    gameDisplay.fill(Values.red)
-    box_1.move(box_1.x+x_vel, box_1.y+y_vel, box_1.angle)
+    gameDisplay.fill(Values.blue)
     box_1.draw()
+    background_1.draw()
+
     pygame.display.update()
-    clock.tick(25)
+    clock.tick(40)
 
 pygame.quit()
